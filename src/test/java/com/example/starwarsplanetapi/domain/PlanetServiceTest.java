dcc3c42d2b0bf87933bb04422e6c36fa39a7bc37
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-08-23 14:00:32 -0300

    Feat: Creating a test to check the planet creation failure.

--- src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.starwarsplanetapi.domain;
 
 import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static com.example.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import com.example.starwarsplanetapi.repositories.PlanetRepository;
@@ -29,5 +31,13 @@ public class PlanetServiceTest {
         Planet createdPlanet = planetService.create(PLANET);
 
         assertThat(createdPlanet).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET))
+            .isInstanceOf(RuntimeException.class);
     }
 }
