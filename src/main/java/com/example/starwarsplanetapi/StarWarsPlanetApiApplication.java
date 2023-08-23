881a93f051de0948bc2be2d415a31a47c7a66765
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-08-23 09:28:32 -0300

    Feat: Initializing the project.

---
+++ src/main/java/com/example/starwarsplanetapi/StarWarsPlanetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.starwarsplanetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/starwarsplanetapi/StarWarsPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
