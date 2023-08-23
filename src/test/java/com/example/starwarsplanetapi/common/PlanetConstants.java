51845deebbdf402a6f6856e85e0e5efbab607b3f
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-08-23 14:00:10 -0300

    Feat: Defining a new constant planet to fail the tests.

--- src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
@@ -4,4 +4,6 @@ import com.example.starwarsplanetapi.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
