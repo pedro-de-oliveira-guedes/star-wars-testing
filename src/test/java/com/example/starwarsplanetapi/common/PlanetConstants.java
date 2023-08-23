9fc4a41eab9fe8d396a8e7a0997bf48b43fdd1c2
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-08-23 10:49:03 -0300

    Feat: Creating a test constant.

---
+++ src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package com.example.starwarsplanetapi.common;
+
+import com.example.starwarsplanetapi.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("name", "climate", "terrain");
+}
