7e13b6c6b4809716bdaecf9281b78ea83d73b6db
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-09-12 10:50:44 -0300

    Feat: Defining constants to get planets by id.

--- src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/starwarsplanetapi/common/PlanetConstants.java
@@ -1,9 +1,16 @@
 package com.example.starwarsplanetapi.common;
 
+import java.util.Optional;
+
 import com.example.starwarsplanetapi.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
 
     public static final Planet INVALID_PLANET = new Planet("", "", "");
+
+    public static final Long VALID_ID = 1L;
+    public static final Optional<Planet> VALID_PLANET = Optional.of(PLANET);
+
+    public static final Long INVALID_ID = 2L;
 }
