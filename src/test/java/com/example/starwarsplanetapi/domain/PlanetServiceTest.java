25fd0135f1af6fdea601b35ee89aa5970c9b0b1b
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-09-12 10:52:44 -0300

    Feat: Creating tests for planet getting by id.

--- src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -2,6 +2,9 @@ package com.example.starwarsplanetapi.domain;
 
 import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
 import static com.example.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
+import static com.example.starwarsplanetapi.common.PlanetConstants.VALID_ID;
+import static com.example.starwarsplanetapi.common.PlanetConstants.VALID_PLANET;
+import static com.example.starwarsplanetapi.common.PlanetConstants.INVALID_ID;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import com.example.starwarsplanetapi.repositories.PlanetRepository;
 import com.example.starwarsplanetapi.services.PlanetService;
@@ -39,5 +44,24 @@ public class PlanetServiceTest {
 
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET))
             .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanetById_WithValidId_ReturnsPlanet() {
+        when(planetRepository.findById(VALID_ID)).thenReturn(VALID_PLANET);
+
+        Optional<Planet> foundPlanet = planetService.get(VALID_ID);
+
+        assertThat(foundPlanet).isNotEmpty();
+        assertThat(foundPlanet).isEqualTo(VALID_PLANET);
+    }
+
+    @Test
+    public void getPlanetById_WithInvalidId_ReturnsEmpty() {
+        when(planetRepository.findById(INVALID_ID)).thenReturn(Optional.empty());
+
+        Optional<Planet> foundPlanet = planetService.get(INVALID_ID);
+
+        assertThat(foundPlanet).isEmpty();
     }
 }
