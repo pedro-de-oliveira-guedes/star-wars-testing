60e41268123e96718ac5d54174d0d5a74c33ae11
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-09-28 11:19:47 -0300

    Feat: Defining a GET endpoint to list planets based on filters.

--- src/main/java/com/example/starwarsplanetapi/web/PlanetController.java
+++ src/main/java/com/example/starwarsplanetapi/web/PlanetController.java
@@ -1,5 +1,7 @@
 package com.example.starwarsplanetapi.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.starwarsplanetapi.domain.Planet;
@@ -36,5 +39,14 @@ public class PlanetController {
     public ResponseEntity<Planet> getByName(@PathVariable("name") String planetName) {
         return planetService.findByName(planetName).map(planet -> ResponseEntity.ok(planet))
             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Planet>> listPlanets(
+        @RequestParam(required = false, name = "climate") String climate,
+        @RequestParam(required = false, name = "terrain") String terrain
+    ) {
+        List<Planet> planets = planetService.list(climate, terrain);
+        return ResponseEntity.ok(planets);
     }
 }
