826fd8d930dfde8585695bc5feea050913a23972
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-09-28 09:44:06 -0300

    Feat: Creating method to get planet by name.

--- src/main/java/com/example/starwarsplanetapi/repositories/PlanetRepository.java
+++ src/main/java/com/example/starwarsplanetapi/repositories/PlanetRepository.java
@@ -3,7 +3,9 @@ package com.example.starwarsplanetapi.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.starwarsplanetapi.domain.Planet;
+import java.util.Optional;
+
 
 public interface PlanetRepository extends CrudRepository<Planet, Long> {
-
+    public Optional<Planet> findByName(String name);
 }
