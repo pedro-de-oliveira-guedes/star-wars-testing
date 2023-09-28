f63411d26a25ebdf5a03a9f5fa0d5af8f73b15f2
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-09-28 11:18:08 -0300

    Feat: Creating a service to build db queries.

---
+++ src/main/java/com/example/starwarsplanetapi/services/QueryBuilder.java
@@ -0,0 +1,14 @@
+package com.example.starwarsplanetapi.services;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+import com.example.starwarsplanetapi.domain.Planet;
+
+public class QueryBuilder {
+    public static Example<Planet> makeQuery(Planet planet) {
+        ExampleMatcher queryMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
+
+        return Example.of(planet, queryMatcher);
+    }
+}
