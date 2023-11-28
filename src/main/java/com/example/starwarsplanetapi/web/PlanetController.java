2a71eaafe5c2bb383be5004d9418ff649f57e10b
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-11-28 10:37:34 -0300

    Feat: Adding delete by ID endpoint.

--- src/main/java/com/example/starwarsplanetapi/web/PlanetController.java
+++ src/main/java/com/example/starwarsplanetapi/web/PlanetController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,12 @@ public class PlanetController {
     ) {
         List<Planet> planets = planetService.list(climate, terrain);
         return ResponseEntity.ok(planets);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removePlanetById(@PathVariable("id") Long id) {
+        planetService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
