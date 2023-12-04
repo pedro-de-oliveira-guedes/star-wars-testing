6cb748125611e4d30a47546ddd8acc371d6c690e
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-12-04 09:39:16 -0300

    Feat: Testing ID getting.

--- src/test/java/com/example/starwarsplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/example/starwarsplanetapi/domain/PlanetRepositoryTest.java
@@ -4,6 +4,7 @@ import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,6 +19,12 @@ public class PlanetRepositoryTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+
+    @AfterEach
+    public void tearDown() {
+        planetRepository.deleteAll();
+        PLANET.setId(null);
+    }
 
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
@@ -47,5 +54,24 @@ public class PlanetRepositoryTest {
         planet.setId(null);
 
         assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+
+        Planet foundPlanet = planetRepository.findById(planet.getId()).orElse(null);
+
+        assertThat(foundPlanet).isNotNull();
+        assertThat(foundPlanet.getName()).isEqualTo(PLANET.getName());
+        assertThat(foundPlanet.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(foundPlanet.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void getPlanet_ByNonExistingId_ReturnsNull() {
+        Planet foundPlanet = planetRepository.findById(1L).orElse(null);
+
+        assertThat(foundPlanet).isNull();
     }
 }
--- src/test/java/com/example/starwarsplanetapi/web/PlanetControllerTest.java
+++ src/test/java/com/example/starwarsplanetapi/web/PlanetControllerTest.java
@@ -3,9 +3,13 @@ package com.example.starwarsplanetapi.web;
 import static com.example.starwarsplanetapi.common.PlanetConstants.PLANET;
 import static com.example.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,5 +74,24 @@ public class PlanetControllerTest {
             .content(objectMapper.writeValueAsString(PLANET))
         )
             .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
+        when(planetService.get(anyLong())).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(
+            get("/planets/{id}", 1L)
+        )
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectMapper.writeValueAsString(PLANET)));
+    }
+
+    @Test
+    public void getPlanet_ByNonExistingId_ReturnsNotFound() throws Exception {
+        mockMvc.perform(
+            get("/planets/{id}", 1L)
+        )
+            .andExpect(status().isNotFound());
     }
 }
