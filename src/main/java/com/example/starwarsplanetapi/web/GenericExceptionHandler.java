732eccdef3ccf725c6b3b6a13f39376cbbc30fe2
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2023-12-04 09:16:34 -0300

    Feat: Creating an exception handler.

---
+++ src/main/java/com/example/starwarsplanetapi/web/GenericExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.starwarsplanetapi.web;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GenericExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex,
+        HttpHeaders headers,
+        HttpStatusCode status,
+        WebRequest request
+    ) {
+        return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<Object> handleDataIntegrityViolationException(
+        DataIntegrityViolationException ex,
+        WebRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(ex.getMessage());
+    }
+}
